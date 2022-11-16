9101a697d809621f7efb24efb114ccc59fddc14c
e232755 <e232755@sbl.ch>
2022-11-16 14:46:07 +0100

    change color of player

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function handleKeyPress(ev) {
+    const player = document.querySelector("#player");
+    console.log(ev)
     if (ev.code === "Space") {
-    const player = document.querySelector("#player")
-    player.setAttribute("cx", 100 * Math.random())
-    player.setAttribute("cy", 100 * Math.random())
-    }
+        player.setAttribute("cx", 100 * Math.random());
+        player.setAttribute("cy", 100 * Math.random());
+    }   else if (ev.key === "r")
+    player.setAttribute("fill", "red")
+
 }
 
 
 
-window.onkeydown = handleKeyPress
+window.onkeydown = handleKeyPress;
