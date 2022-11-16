5d6e7e9ea0b5b1bd56f0e8db9b5c2e0b9167f490
e232755 <e232755@sbl.ch>
2022-11-16 14:49:43 +0100

    neu update

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ function handleKeyPress(ev) {
     if (ev.code === "Space") {
         player.setAttribute("cx", 100 * Math.random());
         player.setAttribute("cy", 100 * Math.random());
-    }   else if (ev.key === "r")
+    }   else if (ev.key === "r"){
     player.setAttribute("fill", "red")
+    }   else if (ev.key === "g") {
+            player.setAttribute("fill", "yellow")
+    }
 
 }
 
