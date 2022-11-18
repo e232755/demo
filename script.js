b911690f26a50bafad675e40fb3b98d1d38c715a
e232755 <e232755@sbl.ch>
2022-11-18 08:26:04 +0100

    added jumping

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function handleKeyPress(ev) {
     const player = document.querySelector("#player");
     console.log(ev)
     if (ev.code === "Space") {
-        player.setAttribute("cx", 100 * Math.random());
-        player.setAttribute("cy", 100 * Math.random());
+        dy = -1
     }   else if (ev.key === "r"){
     player.setAttribute("fill", "red")
     }   else if (ev.key === "g") {
