1ee924049e7057db3d65c938d26fe6de4e3094bb
e232755 <e232755@sbl.ch>
2022-11-18 09:06:15 +0100

    items

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function handleKeyPress(ev) {
     if (ev.code === "Space") {
         player.setAttribute("cx", 100 * Math.random());
         player.setAttribute("cy", 100 * Math.random());
+        spawnItem()
     }   else if (ev.key === "r"){
     player.setAttribute("fill", "red")
     }   else if (ev.key === "g") {
@@ -12,6 +13,15 @@ function handleKeyPress(ev) {
 
 }
 
+function spawnItem() {
+    const NS = "http://www.w3.org/2000/svg"
+    const item = document.createElementNS(NS, "circle")
+    item.setAttribute("cx", 100 * Math.random())
+    item.setAttribute("cy", 20 * Math.random() + 80)
+    item.setAttribute("r", 2)
+    item.setAttribute("fill", "black")
+    document.querySelector("svg").appendChild(item)
+}
 
 
 window.onkeydown = handleKeyPress;
